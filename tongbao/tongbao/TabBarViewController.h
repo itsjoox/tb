8dd7518eac3cc3378349c7682b0fd4f42e81bc5e
ZX <ZX@zx-2.local>
2016-03-11 23:48:21 +0800

    Add TabBarViewController

---
+++ tongbao/tongbao/TabBarViewController.h
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.h
+//  tongbao
+//
+//  Created by ZX on 16/3/11.
+//  Copyright © 2016年 ZX. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+//#import "SubOrderDtlViewController.h"
+
+
+@interface TabBarViewController : UITabBarController
+
+
+
+@end
---
+++ tongbao/tongbao/TabBarViewController.m
@@ -0,0 +1,26 @@
+//
+//  TabBarViewController.m
+//  tongbao
+//
+//  Created by ZX on 16/3/11.
+//  Copyright © 2016年 ZX. All rights reserved.
+//
+
+#import "TabBarViewController.h"
+
+@interface TabBarViewController ()
+
+@end
+
+@implementation TabBarViewController
+
+- (void)viewDidLoad {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
