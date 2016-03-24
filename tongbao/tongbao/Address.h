7c2d3f5452bc5bbab3db0a85df0b80f861ac32b6
ZX <ZX@zx-2.local>
2016-03-25 00:47:55 +0800

    add Address model

---
+++ tongbao/tongbao/Address.h
@@ -0,0 +1,19 @@
+//
+//  Address.h
+//  tongbao
+//
+//  Created by ZX on 16/3/25.
+//  Copyright © 2016年 ZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+
+@interface Address : NSObject
+@property (copy, nonatomic) NSString *name;
+@property (copy, nonatomic) NSString *lat;
+
+@property (copy, nonatomic) NSString *lng;
+
+@end
---
+++ tongbao/tongbao/Address.m
@@ -0,0 +1,19 @@
+//
+//  Address.m
+//  tongbao
+//
+//  Created by ZX on 16/3/25.
+//  Copyright © 2016年 ZX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Address.h"
+@interface Address()
+
+@end
+
+@implementation Address
+
+
+
+@end
