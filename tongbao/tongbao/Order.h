3e7e2d60a9dfec9cc5205f1338c8a47df4cae601
ZX <ZX@zx-2.local>
2016-03-24 17:59:49 +0800

    fix order model

--- tongbao/tongbao/Order.h
+++ tongbao/tongbao/Order.h
@@ -10,9 +10,6 @@
 #import <Foundation/Foundation.h>
 
 
-//"type":(账单类型)0表示充值 1表示提现 2表示支付 3表示退款 4表示到帐, "time":(交易时间), "money":12(金额),
-//"order"(只有type为2,3,4时才有该值):{"id":1,"addressFrom":开始地点,"addressTo":终止地点}
-
 @interface Order : NSObject
 
 @property (copy, nonatomic) NSString* addressFrom ;
